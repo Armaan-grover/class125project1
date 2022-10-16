39a16bb64e51c5176b5049be6701a0e4eef9f1c0
Armaan-grover <97002933+Armaan-grover@users.noreply.github.com>
2022-10-15 19:29:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(500,400)
+canvas.center()
+video = createCapture(VIDEO)
+video.hide()
+
+}
+
+function draw(){
+image(video,0,0,500,400)
+}
